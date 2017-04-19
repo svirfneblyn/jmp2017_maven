5d5ae00abc901ecdaaeb27818e7794c56cf02953
svirfneblyn <svirfneblyn@gmail.com>
2017-04-19 07:53:53 +0300

    added war plugin

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -1,12 +1,20 @@
 package com;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+@EnableAutoConfiguration
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
     }
 }
