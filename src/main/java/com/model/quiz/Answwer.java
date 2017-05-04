76f945f1737c0bd8a6c7889ba99b6fb98fb906c2
svirfneblyn <svirfneblyn@gmail.com>
2017-05-04 07:58:00 +0300

    created entitys for JPA task

---
+++ src/main/java/com/model/quiz/Answwer.java
@@ -0,0 +1,7 @@
+package com.model.quiz;
+
+/**
+ * Created by User on 5/4/2017.
+ */
+public class Answwer {
+}
--- src/main/java/com/resource/iresource/IDispatcherResource.java
+++ src/main/java/com/resource/iresource/IDispatcherResource.java
@@ -3,10 +3,7 @@ package com.resource.iresource;
 import com.dto.DispatcherDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,7 +13,7 @@ public interface IDispatcherResource {
     @RequestMapping(value = "/dispatcher", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
     ResponseEntity<?> addDispatcher(@RequestBody DispatcherDto dispatcherDto);
 
-    @RequestMapping(value = "/dispatcher", method = RequestMethod.GET, produces = "application/json")
+    @GetMapping(value = "/dispatcher", produces = "application/json")
     List<DispatcherDto> getDispatchers();
 
     @RequestMapping(value = "/dispatcher/{name}", method = RequestMethod.PUT, consumes = "application/json", produces = "application/json")
